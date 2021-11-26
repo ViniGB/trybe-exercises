35119885a595246b97f1e77899d13d4faefe11be
Vinicius Gomes <viniciusemog@hotmail.com>
2021-11-26 18:33:49 -0300

    Resoluçao do requisito 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/requisito3.js
@@ -0,0 +1,13 @@
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+const a = 10;
+const b = 30;
+const c = 99;
+
+if (a >= b && a >= c) {
+  console.log(a);
+} else if (b >= c) { 
+  console.log(b);
+} else {
+  console.log(c);
+}
