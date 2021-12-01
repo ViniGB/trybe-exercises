1d55b19f09c9b15c8710ba388a86bb4755251624
Vinicius Gomes <viniciusemog@hotmail.com>
2021-12-01 17:59:51 -0300

    Resoluçao do requisito 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/part-1/requisito1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/part-1/requisito1.js
@@ -1,4 +1,4 @@
-// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 
 let info = {
   personagem: 'Margarida',
