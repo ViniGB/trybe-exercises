faf89e654eb482d70cd3f199dd4ac0f80764443e
Vinicius Gomes <viniciusemog@hotmail.com>
2021-12-09 01:57:33 -0300

    Text Color check

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,3 +1,4 @@
+// Setting Background Color
 const clickBackgroundColor = document.getElementById('background');
 
 function changeBackgroundColor () {
@@ -11,10 +12,37 @@ function changeBackgroundColor () {
 
 clickBackgroundColor.addEventListener('click', changeBackgroundColor);
 
+// Setting Text Color
+const clickTextColor = document.getElementById('text');
+
+function changeTextColor () {
+
+  const inputText = document.getElementById('textColor');
+  const textColor = document.getElementById('paragraphtext');
+  const h1Color = document.getElementById('headertitle');
+  textColor.style.color = inputText.value;
+  h1Color.style.color = inputText.value;
+  const textColorStorage = inputText.value;
+  const h1ColorStorage = inputText.value;
+  localStorage.setItem('textColor', JSON.stringify(textColorStorage));
+  localStorage.setItem('h1Color', JSON.stringify(h1ColorStorage));
+};
+
+clickTextColor.addEventListener('click', changeTextColor);
+
+// Setting onload config
 function onLoad () {
   const body = document.body;
   const backgroundColor = JSON.parse(localStorage.getItem('bgColor'));
   body.style.backgroundColor = backgroundColor;
+
+  const text = document.getElementById('paragraphtext');
+  const textColor = JSON.parse(localStorage.getItem('textColor'));
+  text.style.color = textColor;
+
+  const h1 = document.getElementById('headertitle');
+  const h1Color = JSON.parse(localStorage.getItem('h1Color'));
+  h1.style.color = h1Color;
 };
 
 window.onload = function() {
