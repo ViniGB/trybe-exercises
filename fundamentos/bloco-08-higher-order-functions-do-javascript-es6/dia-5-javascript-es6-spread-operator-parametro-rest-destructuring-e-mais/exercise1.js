46fab922cb7b239be74c1ecdc70fe2d6b9c656ad
Vinicius Gomes <viniciusemog@hotmail.com>
2022-01-24 16:44:18 -0300

    Exercise 4

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
@@ -1,3 +1,6 @@
+// 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+// Dica: use spread operator .
+
 const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
