1a59e527180bb180080b3bcb6580c07f81ab6659
Vinicius Gomes <viniciusemog@hotmail.com>
2022-02-16 20:32:25 -0300

    Adiçao do Content, resoluçao da parte 2 do requisito 3

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/to-learn-content/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/to-learn-content/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Content />
+    </div>
   );
 }
 
